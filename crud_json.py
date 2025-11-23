b9f90fc6ccd55c0a194c8e887cdd2e5f997f2e55
shfqt255 <shfqt25@gmail.com>
2025-11-23 23:37:26 +0500

    feat(crud_json): add HTTP exception handling and sorting - Added HTTP exception handling and sorting in crud_json.py - Added HttpMethod.txt, HttpStatus.txt, and queryParameters.py

--- crud_json.py
+++ crud_json.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app=FastAPI()
@@ -20,4 +20,17 @@ def  readSpecificPateint(pateint_id: str=Path(..., description="Patient Id in DB
    if pateint_id in data:
       return data[pateint_id]
    else:
-      return "An Error occured"
+      raise HTTPException(status_code=404, details="Not found")
+   
+@app.get("/sort")
+def sort(sort_by, order):
+   sort_by2=["height", "weight", "bmi", "age"]
+   order2=["asc", "dsc"]
+   if sort_by not in sort_by2:
+      raise HTTPException(status_code=400, details="Inavlid Request")
+   if order not in order2:
+      raise HTTPException(status_code=400, details="Inalid request")
+   data=load_data()
+   reverse= False if order!="dsc" else True 
+   sorted_data=sorted(data.values(), key= lambda x: x.get(sort_by, 0), reverse=reverse)
+   return sorted_data
