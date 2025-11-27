0becc8c8432bd45589e09d6d68e1c6137c6e1fc2
shfqt255 <shfqt25@gmail.com>
2025-11-27 22:53:29 +0500

    feat: Added Specific Validation
    EmailStr, AnyUrl, Field, and from typing List, Dict,Optional, Annotated are used

--- Pydantic_why.py
+++ Pydantic_why.py
@@ -1,19 +1,29 @@
-from pydantic import BaseModel
-class info(BaseModel):
-    name: str
-    age: int
-    regestration:int
+from pydantic import BaseModel, EmailStr, AnyUrl,  Field
+from typing import List, Dict, Optional, Annotated
 
-def print_data(Info:info):
+class Patientinfo(BaseModel):
+    name: str= Field(max_length=50)
+    age: int=Field(gt=0, lt=150)
+    email:EmailStr 
+    Weight: Annotated[float, Field( strict=True, title='Weight of the patient', description='The must be number because we used strict ', examples=['Do not write 5 with colon'])]
+    allergies: Optional[List[str]]=None
+    Other_details: Dict[str, str]
+
+def print_data(Info: Patientinfo):
     print(Info.name)
     print(Info.age)
-    print(Info.regestration)
+    print(Info.email)
+    print(Info.Other_details)
+    print(Info.allergies)
 
-given_info={
+given_info = {
+    'Weight': 72.5, #order does not matter. 
     'name': 'shafqat ullah',
     'age': 30,
-    'regestration':25
+    'email': 'abd@gmail.com',
+    'Other_details': {'phone': '1284848', 'email': 'shafqat@example.com'},
+    # 'allergies': ['pollen', 'dust']
 }
 
-info1=info(**given_info)
+info1 = Patientinfo(**given_info)
 print_data(info1)
